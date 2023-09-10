d9cedf6812791d25ec027f86f34befcb9c450133
Nicolai Lindner <nicolai.emanuel.lindner@gmail.com>
2023-09-10 15:04:05 +0200

    "Added chakra UI and added the header with sidebar"

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,8 @@
-import Image from 'next/image'
-import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div>
-      <Button> Click me </Button>
+      <h1> Hello World! </h1>
     </div>
   )
 }
